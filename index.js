25181dd55fa1efa908aaaa2690bf512f7691a0a1
David Chou <davidchou2009@gmail.com>
2022-01-31 22:31:43 -0800

    add prompt to choose initial selection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const inquirer = require('inquirer');
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -8,3 +9,25 @@ const db = mysql.createConnection({
     database: 'employee_db'
 });
 
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: "Select an option below: ",
+                name: 'choice',
+                choices: ['View all departments','View all roles','View all employees','Add a department','Add a role','Add an employee','Update employee role']
+            }
+
+        ])
+        .then((response) => {
+            // const manager = new Manager(response.name, response.id, response.email, response.number);
+            // teamCards.push(manager);
+            // console.log('manager: ',manager);
+            // addEmployee();
+            console.log(response);
+
+        });
+
+}
+init();
