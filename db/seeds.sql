6e709631705bf32db0734e99c26ee298358c091d
David Chou <davidchou2009@gmail.com>
2022-01-31 22:11:59 -0800

    add data to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,16 @@
-INSERT INTO department (dept_name)
-VALUES ("Research & Development"),
-       ("Human Resources");
+INSERT INTO departments (department)
+VALUES 
+    ("Research & Development"),
+    ("Human Resources");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+    ("Engineer","100000","1"),
+    ("Office Manager","70000","2");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+    ("Farley","Bacon","2","1"),
+    ("Chicken","Squiggles","1","1");
+
+
