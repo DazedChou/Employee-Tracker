27ddfff8204c9471370d0d75c9bb8ef7247ea355
David Chou <davidchou2009@gmail.com>
2022-01-31 22:24:01 -0800

    establish connection between js and mysql

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'employee_db'
+});
+
